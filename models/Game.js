49a59030efc44b74a2f32496bacbe84df076c468
favour <omojolaobaloluwa@gmail.com>
2025-10-16 23:40:54 +0100

    done

--- models/Game.js
+++ models/Game.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
 
-const GameSchema = new mongoose.Schema({
-  tipTitle: { type: String, required: true },
-  tipPrice: { type: Number, required: true },
-  oddRatio: { type: Number, required: true },
-  bettingType: { type: String, required: true },
-  bettingSites: { type: [String], required: true }, // assuming an array of betting sites
-  confidenceLevel: { type: String, required: true }, // you could also use Number if it's a rating
-  contentAfterPurchase: { type: String, required: true },
-  duration: { type: Number, required: true }, // assuming duration is in minutes or hours
-  purchaseLimit: { type: Number },
-  CurrentLimit: { type: Number, default: 0 },
-  active: { type: Boolean, default: false },
-  purchasedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
+const GameSchema = new mongoose.Schema(
+  {
+    tipTitle: { type: String, required: true },
+    tipPrice: { type: Number, required: true },
+    oddRatio: { type: Number, required: true },
+    bettingType: { type: String },
+    bettingSites: { type: [String], required: true }, // assuming an array of betting sites
+    confidenceLevel: { type: String, required: true }, // you could also use Number if it's a rating
+    contentAfterPurchase: { type: String, required: true },
+    duration: { type: Number, required: true }, // assuming duration is in minutes or hours
+    purchaseLimit: { type: Number },
+    CurrentLimit: { type: Number, default: 0 },
+    active: { type: Boolean, default: false },
+    status: { type: String, default: "Pending" },
+    purchasedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Game", GameSchema);
